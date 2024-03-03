8f1f8c1f8a920290f089b971431cb2b0f327bf93
jiooong <lucy991010@gmail.com>
2024-03-04 00:12:08 +0900

    #3 Feat : User엔티티 생성 및 엔티티의 생성/수정 시각 자동 기록

---
+++ src/main/java/com/example/bank/domain/user/entity/UserRoleEnum.java
@@ -0,0 +1,18 @@
+package com.example.bank.domain.user.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRoleEnum {
+    USER(Authority.USER),
+    ADMIN(Authority.ADMIN);
+
+    private final String authority;
+
+    UserRoleEnum(String authority){ this.authority = authority;}
+
+    private static class Authority{
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
+}
