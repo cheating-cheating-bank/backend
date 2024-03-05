6531de672b73bc37fbbd1946729d636c35c8b32d
SG1515 <sunggu507@naver.com>
2024-03-05 22:05:09 +0900

    Feat : Account Entity

--- src/main/java/com/example/bank/domain/account/entity/Account.java
+++ src/main/java/com/example/bank/domain/account/entity/Account.java
@@ -1,6 +1,32 @@
 package com.example.bank.domain.account.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Account {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long number; //계좌번호
+
+    @Column
+    private Long password; //비밀번호
+
+    @Column
+    private Long balance; //잔액
+
+    @Column
+    private LocalDateTime createAt; // 생성일시
+
+    @Column
+    private LocalDateTime datetime; // 수정일시
 
 }
