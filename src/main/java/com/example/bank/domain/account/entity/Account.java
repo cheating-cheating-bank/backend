192301a41192a0f42626c212c021f9c0c8c4ae86
SG1515 <sunggu507@naver.com>
2024-03-06 21:36:44 +0900

    Feat : Account Entity에 Auditing 적용 및 Column 조건 추가

--- src/main/java/com/example/bank/domain/account/entity/Account.java
+++ src/main/java/com/example/bank/domain/account/entity/Account.java
@@ -3,30 +3,37 @@ package com.example.bank.domain.account.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Table(name = "account_tb")
+@EntityListeners(AuditingEntityListener.class)
 public class Account {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(nullable = false, unique = true, length = 20)
     private Long number; //계좌번호
 
-    @Column
+    @Column(nullable = false, length = 4)
     private Long password; //비밀번호
 
-    @Column
+    @Column(nullable = false)
     private Long balance; //잔액
 
-    @Column
+    @CreatedDate // insert
+    @Column(updatable = false)
     private LocalDateTime createAt; // 생성일시
 
+    @LastModifiedDate // Insert, update
     @Column
-    private LocalDateTime datetime; // 수정일시
+    private LocalDateTime updateAt; // 수정일시
 
 }
